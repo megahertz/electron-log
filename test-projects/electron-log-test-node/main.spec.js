a6def44f537548a3fb1f7d08b80f221156ae4fbb
Linus Färnstrand <linus@mullvad.net>
2017-09-06 19:19:10 +0200

    Fix tests to check for 12 chars, not 13

--- test-projects/electron-log-test-node/main.spec.js
+++ test-projects/electron-log-test-node/main.spec.js
@@ -9,7 +9,7 @@ describe('node test project', () => {
   it('should write one line to a log file', () => {
     return helper.run(APP_NAME).then((logs) => {
       expect(logs.length).to.equal(2);
-      expect(logs[0]).to.match(/\[[\d-]{10} [\d:]{13}] \[warn] node warn/);
+      expect(logs[0]).to.match(/\[[\d-]{10} [\d:.]{12}] \[warn] node warn/);
     });
   })
 });
