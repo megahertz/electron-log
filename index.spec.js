da22ad5aaac055074af6adc5a24300b944ca3eee
Joshua Westerheide <dev@jdoubleu.de>
2018-10-16 21:00:09 +0200

    Add tests for named loggers

--- index.spec.js
+++ index.spec.js
@@ -5,10 +5,26 @@ var index  = require('./index');
 
 
 describe('package', function() {
+  var levels = ['error', 'warn', 'info', 'verbose', 'debug', 'silly'];
+
   it('should contains npm level methods', function() {
-    var levels = ['error', 'warn', 'info', 'verbose', 'debug', 'silly'];
     levels.forEach(function (level) {
       expect(index[level]).to.be.a('function');
     });
+  });
+
+  it('should contains all log leve methods in named logger', function() {
+    var logger = index.createNamedLogger('SomeLogger')
+    levels.forEach(function (level) {
+      expect(logger[level]).to.be.a('function');
+    });
+  });
+
+  it('should have distinct config for any named logger', function () {
+    var logger = index.createNamedLogger('SomeLogger')
+    index.transports.someVal = true
+    expect(logger.transports.someVal).be.an('undefined')
+    logger.transports.otherVal = 12
+    expect(index.transports.otherVal).be.an('undefined')
   });
 });
