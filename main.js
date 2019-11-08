63f4b009cba0a90ca6c2a590d3bbe532b2b453e8
Alexey Prokhorov <megahertz@l96.ru>
2019-11-08 14:06:49 +0500

    Remove deprecated entry points

--- main.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = require('./index');
-console.warn('electron-log/main is deprecated and will be removed in v4');
--- renderer.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = require('./index');
-console.warn('electron-log/renderer is deprecated and will be removed in v4');
