9a8c5fe9ee468e59d5738a752791f33aa6432ac3
Alexey Prokhorov <megahertz@l96.ru>
2019-11-19 19:27:30 +0500

    ipc: Console level does not affect mainConsole / rendererConsole
    Closes #138

--- src/transports/ipc.js
+++ src/transports/ipc.js
@@ -2,6 +2,7 @@
 
 var format = require('../format');
 var electronApi = require('../electronApi');
+var log = require('../log.js');
 
 var IPC_EVENT = '__ELECTRON_LOG_TRANSPORT_IPC__';
 
@@ -11,16 +12,20 @@ function ipcTransportFactory(electronLog) {
   transport.level = electronLog.isDev ? 'silly' : false;
   transport.format = '[{h}:{i}:{s}.{ms}] {text}';
 
-  electronApi.onIpc(IPC_EVENT, function (event, msg) {
-    electronLog.transports.console(msg);
+  electronApi.onIpc(IPC_EVENT, function (_, message) {
+    log.runTransport(
+      electronLog.transports.console,
+      message,
+      electronLog
+    );
   });
 
   electronApi.loadRemoteModule('electron-log');
 
   return electronApi.isElectron() ? transport : null;
 
-  function transport(msg) {
-    msg.data = msg.data.map(format.stringifyObject);
-    electronApi.sendIpc(IPC_EVENT, msg);
+  function transport(message) {
+    message.data = message.data.map(format.stringifyObject);
+    electronApi.sendIpc(IPC_EVENT, message);
   }
 }
