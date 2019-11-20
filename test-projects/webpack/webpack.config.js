fc40ecf71fce2ce07ac1b88189631b3fe1573d79
Alexey Prokhorov <megahertz@l96.ru>
2019-11-20 16:51:24 +0500

    e2e: Remove babel dev dependency

--- test-projects/webpack/webpack.config.js
+++ test-projects/webpack/webpack.config.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var presetEnv = require('@babel/preset-env');
 var path = require('path');
 
 module.exports = [
@@ -9,18 +8,6 @@ module.exports = [
     entry: ['./src/main.js'],
     output: { filename: 'main.js' },
     target: 'electron-main',
-    module: {
-      rules: [
-        {
-          test: /.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          options: {
-            presets: [presetEnv],
-          },
-        },
-      ],
-    },
     node: {
       __dirname: false,
     },
@@ -36,18 +23,6 @@ module.exports = [
     entry: ['./src/renderer.js'],
     output: { filename: 'renderer.js' },
     target: 'electron-renderer',
-    module: {
-      rules: [
-        {
-          test: /.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          options: {
-            presets: [presetEnv],
-          },
-        },
-      ],
-    },
     resolve: {
       alias: {
         'electron-log': path.resolve('../..'),
