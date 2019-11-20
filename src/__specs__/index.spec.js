94e5c5c3734e59c3e05b61c1ba12a611ac62f222
Alexey Prokhorov <megahertz@l96.ru>
2019-11-20 16:12:58 +0500

    main: Add test for multiple instances

--- src/__specs__/index.spec.js
+++ src/__specs__/index.spec.js
@@ -9,4 +9,14 @@ describe('index', function () {
       expect(typeof index[level]).toBe('function');
     });
   });
+
+  it('should create independent logger instance', function () {
+    var defaultLogger = index;
+    var customLogger = index.create('custom');
+
+    customLogger.transports.file.fileName = 'custom.log';
+
+    expect(defaultLogger.transports.file.fileName).toBe('main.log');
+    expect(customLogger.transports.file.fileName).toBe('custom.log');
+  });
 });
