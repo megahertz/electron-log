511329ff9b2f1828a06b86a89188510340b958bc
Alexey Prokhorov <megahertz@l96.ru>
2020-11-02 10:09:06 +0500

    ci: Skip linting for node < 10 (#221)
    * ci: Check node version
    
    * ci: Run only unit tests for node < 10
    
    * ci: Install electron only for node >= 8
    
    * ci: Set min node version to 8

--- src/__specs__/utils/node-check.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-var nodeVersion = parseFloat(process.version.replace('v', ''));
-var requiredVersion = parseFloat(process.argv[2]);
-
-process.exit(nodeVersion >= requiredVersion ? 0 : 1);
